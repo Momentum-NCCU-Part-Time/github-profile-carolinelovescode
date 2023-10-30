730b1c98a6a8503cd749ffda6c014ee689cac01b
carolinelovescode <csuec23@gmail.com>
2023-10-30 18:13:01 -0400

    with css and updated js

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const containerOfAll = document.querySelector('.container');
 let userName = document.getElementById('username');
 let userEmail = document.getElementById('userEmail');
 let userLoc = document.getElementById('userLoc');
-let github = document.getElementById('')
+let github = document.getElementById('githubLink')
 
-let repo = document.getElementById('repo');
+// let repo = document.getElementById('repo');
 /* let repo1 = document.getElementById('repo1')
 let repo2 = document.getElementById('repo2') */
 
@@ -19,23 +19,39 @@ fetch(yourURL).then((response) => {
     userName.innerText = ` Username: ${parsedJsonResponse.login}`
     // userEmail.innerText = `Email: csuec23@gmail.com`
     userLoc.innerText = `Location: ${parsedJsonResponse.location}`
-    repo.src = `${parsedJsonResponse.repos_url}`
+    // repo.src = `${parsedJsonResponse.repos_url}`
 });
 
 fetch(repoURL).then((response) => {
     return response.json();
 }).then((parsedJsonResponse) => {
-    console.log(parsedJsonResponse)
 
-    // let repo1 = document.createElement('li');
-    // let repoLinx1 = document.createElement('a');
+    let allRepo = document.getElementById('repo');
+    let repos = document.createElement('ul');
+    let repo1 = document.createElement('li');
+    let repoLinx1 = document.createElement('a');
+    let repo2 = document.createElement('li');
+    let repoLinx2 = document.createElement('a');
 
-    let repo1 = document.getElementById('repo1')
-    repo1.innerText = parsedJsonResponse[1]["full_name"]
-    repoLinx1.src = parsedJsonResponse[1]['html_url']
+    repoLinx1.innerText = parsedJsonResponse[1]['full_name'];
+    repoLinx1.href = parsedJsonResponse[1]['html_url'];
 
-    // repo1.appendChild(repo);
-    // repoLinx1.appendChild(repo);
+    repo1.appendChild(repoLinx1);
+    repos.appendChild(repo1);
+    allRepo.appendChild(repos)
+
+
+    repoLinx2.innerText = parsedJsonResponse[0]['full_name']
+    repoLinx2.href = parsedJsonResponse[0]['html_url']
+
+    repo2.appendChild(repoLinx2);
+    repos.appendChild(repo2);
+    allRepo.appendChild(repos)
+
+    repos.classList.add('repoList')
+
+    /* repo2.appendChild(repos)
+    document.body.appendChild(repo2); */
 
 
 })
